12d4a560054fdee3a44a4a8120b4ef3401f7b9f0
dayFun <joethorngren@gmail.com>
2015-04-02 20:57:09 -0500

    Fucking dependencies, build path, shit, something something.

--- src/main/java/model/Asset.java
+++ src/main/java/model/Asset.java
@@ -2,13 +2,33 @@ package model;
 
 public class Asset {
 
+    private String name;
+    private double cost;
+    private double salvageValue;
+    private int life;
+    private double endValue;
+
     public Asset(String name, double cost, double salvageValue, int life) {
-        // TODO Auto-generated constructor stub
+        this.name = name;
+        this.cost = cost;
+        this.salvageValue = salvageValue;
+        this.life = life;
     }
 
     public double getAnnualDep() {
-        // TODO Auto-generated method stub
-        return 0;
+        return (cost - salvageValue) / life;
+    }
+
+    public double getAnnualDep(int year) {
+        return 2 * getAnnualDep() * salvageValue;
+    }
+
+    public double getEndValue() {
+        return endValue;
+    }
+
+    public void setEndValue(double endValue) {
+        this.endValue = endValue;
     }
 
 }
--- src/test/java/model/AssetTest.java
+++ src/test/java/model/AssetTest.java
@@ -1,16 +1,26 @@
 package model;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class AssetTest {
 
     private Asset asset;
 
+    @Before
+    public void setup() {
+        asset = new Asset("Truck", 30000, 5000, 5);
+    }
+
     @Test
     public void testStraightLineGetAnnualDepreciation() throws Exception {
-        asset = new Asset("Truck", 30000, 5000, 5);
+        assertEquals(5000, asset.getAnnualDep(), 0.0001);
+    }
 
-        assertThat(asset.getAnnualDep(), is(equalTo(5000)));
-
+    @Test
+    public void testDoubleDeclineGetAnnualDepreciation() throws Exception {
+        assertEquals(12000, asset.getAnnualDep(5), 0.0001);
     }
 }
